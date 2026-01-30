998a804bcfbd1a363bfe23f419c6504eae4e0777
YogitaKanaki <yogitakanaki20@gmail.com>
2026-01-30 15:14:01 +0530

    Fixed Navigation

--- src/pages/CreatePaste.jsx
+++ src/pages/CreatePaste.jsx
@@ -1,32 +1,33 @@
 import api from "../api/axios";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom"; 
+import { useNavigate } from "react-router-dom";
 
 export default function CreatePaste() {
   const [content, setContent] = useState("");
   const [ttl, setTtl] = useState("");
   const [views, setViews] = useState("");
-  const [url, setUrl] = useState(""); 
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
+
   const submit = async () => {
-  try {
-    const res = await api.post("/api/pastes", {
-      content,
-      ttl_seconds: ttl ? Number(ttl) : null,
-      max_views: views ? Number(views) : null
-    });
+    try {
+      if (!content.trim()) {
+        alert("Content cannot be empty");
+        return;
+      }
 
-    const pasteId = res.data.id;
-    navigate(`/p/${pasteId}`);
-    setUrl(`${window.location.origin}/p/${pasteId}`);
-  } catch (err) {
-    console.error(err);
-    alert(err?.response?.data?.error || "Failed to create paste");
-    console.log("API BASE:", process.env.REACT_APP_API_BASE_URL);
-  }
-};
+      const res = await api.post("/api/pastes", {
+        content,
+        ttl_seconds: ttl ? Number(ttl) : null,
+        max_views: views ? Number(views) : null
+      });
 
-
+      // Redirect to shareable URL (/p/:id)
+      navigate(`/p/${res.data.id}`);
+    } catch (err) {
+      console.error(err);
+      alert(err?.response?.data?.error || "Failed to create paste");
+    }
+  };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
@@ -37,18 +38,21 @@ export default function CreatePaste() {
           className="w-full border p-2 mb-2"
           rows="5"
           placeholder="Paste your content here..."
+          value={content}
           onChange={(e) => setContent(e.target.value)}
         />
 
         <input
           className="w-full border p-2 mb-2"
           placeholder="TTL (seconds)"
+          value={ttl}
           onChange={(e) => setTtl(e.target.value)}
         />
 
         <input
           className="w-full border p-2 mb-2"
           placeholder="Max Views"
+          value={views}
           onChange={(e) => setViews(e.target.value)}
         />
 
@@ -58,21 +62,6 @@ export default function CreatePaste() {
         >
           Create
         </button>
-
-        {url && (
-          <p className="mt-3 text-sm break-all">
-            Share:{" "}
-            <a
-              className="text-blue-500 underline"
-              href={url}
-              target="_blank"
-              rel="noreferrer"
-            >
-              {url}
-            </a>
-          </p>
-          
-        )}
       </div>
     </div>
   );
